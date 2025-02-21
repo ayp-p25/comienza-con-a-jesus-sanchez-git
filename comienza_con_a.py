95906a264a3f487b9ea6ee5cb4a087b8c097f8a0
jesusa-sanchez-git <jesusa.sanchez@iteso.mx_PianoMan11VJ>
2025-02-21 08:04:22 -0600

    First

--- comienza_con_a.py
+++ comienza_con_a.py
@@ -1,15 +1,17 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Ejercicio : comienza co A
+• Jesús Alejandro Sánchez Muñoz 
+• 758377 
+• Igenieria Mecanica 
+• Algoritmos y Programcion 
+• ESI232B2 
+• Jorge Zaldivar 
+• 13/feb/25 
+• 1 hora aprox
 """
 
-# Declaraciones
-CONSTANTE = valor
-
-# Entradas
-entrada = input()
-
-# Proceso
-
-
-# Salidas
-print(salida)
+Palabra = input("Dime una palabra que quisieras saber que empiece con A: \n ").lower()
+if Palabra[0]== "a" : 
+    print("Esta palbra empieza con A ")
+else:
+    print("Esta palbra no empieza con A")
